2593b7ec7a218d5e81f3736add5c810dbf0420b8
Mateusz121702 <72619943+Mateusz121702@users.noreply.github.com>
2022-05-23 10:11:50 +0200

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,24 @@
+public class Add {
+  
+  double x;
+  double y;
+  
+  public double addNumbers(){
+    double result =x +y;
+    return result;
+  }
+  public double subraction (){
+    double result = x-y;
+    return result;
+  }
+  public double multiplication(){
+    double result =x*y;
+    return result;
+  }
+  public double division(){
+    double result=x/y;
+    return result;
+  }
+
+
+}
